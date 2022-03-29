c6687247eef27a61b750d7e2cf2e6cc0740d57d4
Kurt Kerns <kurtkerns20@gmail.com>
2022-03-29 11:37:34 -0500

    [+] exercises completed for class and objs 1

--- oop_book/work.rb
+++ oop_book/work.rb
@@ -1,12 +1,29 @@
-module Foliage
-  def trees
-    puts "We planted a tree!"
+class GoodDog
+  attr_accessor :name, :height, :weight
+
+  def initialize(n, h, w)
+    @name = n
+    @height = h
+    @weight = w
+  end
+
+  def speak
+    "#{name} say arf!"
+  end
+
+  def change_info(n, h, w)
+    self.name = n
+    self.height = h
+    self.weight = w
+  end
+
+  def info
+    "#{self.name} weighs #{self.weight} and is #{self.height} tall."
   end
 end
 
-class Creation
-  include Foliage
-end
+sparky = GoodDog.new('Sparky', '12 inches', '10 lbs')
+puts sparky.info
 
-the_world = Creation.new
-the_world.trees
+sparky.change_info('Spartacus', '24 inches', '45 lbs')
+puts sparky.info
