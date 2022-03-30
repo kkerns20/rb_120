26bdc12f4198556178069e44a1bf83ced75a7e2d
Kurt Kerns <kurtkerns20@gmail.com>
2022-03-30 11:22:57 -0500

    [+] file over private, protected and public method

---
+++ OOP_book/public_private_protected.rb
@@ -0,0 +1,20 @@
+class Animal
+  def a_public_method
+    "Will this work? " + self.a_protected_method
+  end
+
+  protected
+
+  def a_protected_method
+    "Yes, I'm protected!"
+  end
+end
+
+fido = Animal.new
+# from inside the class, protected methods are accessible just like public methods.
+puts fido.a_public_method        # => Will this work? Yes, I'm protected!
+
+# from outside the class, protected methods act just like private methods.
+puts fido.a_protected_method
+  # => NoMethodError: protected method `a_protected_method' called for
+    #<Animal:0x007fb174157110>
