8b09baeed60d6f3694175d21d90d5ce4607a39c3
kkerns20 <kurtkerns20@gmail.com>
2022-04-03 17:53:34 -0500

    [+] solution for easy_1.8

---
+++ oop_exercises/easy_1/8.rb
@@ -0,0 +1,26 @@
+=begin
+- Create a class called `Square` that inherits from Rectangle
+- is also used like:
+    square = Square.new(5)
+    puts "area of square = #{square.area}"
+=end
+
+class Rectangle
+  def initialize(height, width)
+    @height = height
+    @width = width
+  end
+
+  def area
+    @height * @width
+  end
+end
+
+class Square < Rectangle
+  def initialize(side)
+    super(side, side)
+  end
+end
+
+square = Square.new(5)
+puts "area of square = #{square.area}"
