8ca81ee5b074736d02c884273df92b93ca2a7f24
kkerns20 <kurtkerns20@gmail.com>
2022-04-04 12:17:20 -0500

    [+] updated solution for 8.rb

--- oop_exercises/easy_2/8.rb
+++ oop_exercises/easy_2/8.rb
@@ -8,6 +8,7 @@ class Expander
   def to_s
     # can't use `self` with the private instance method expand
     expand(3) # instead of self.expand(3)
+    # as of Ruby 2.7.0, it's legal to use self with a private instance mthod
   end
 
   private
