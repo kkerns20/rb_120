7f3ede4bdbfc828ea4c114c45e53ebf07825db57
kkerns20 <kurtkerns20@gmail.com>
2022-04-06 13:24:13 -0500

    [+] notes over #+ method

--- lesson_03/fake_operators.rb
+++ lesson_03/fake_operators.rb
@@ -30,18 +30,40 @@ class Team
     @members = []
   end
 
+  # def <<(person)
+  #   return if person.not_yet_18?              # suppose we had Person#not_yet_18?
+  #   members.push person
+  # end
+  # we can build in a guard clause to reject adding members unless some criteria is met
   def <<(person)
-    return if person.not_yet_18?              # suppose we had Person#not_yet_18?
     members.push person
   end
-  # we can build in a guard clause to reject adding members unless some criteria is met
+
+  def +(other_team)
+    temp_team = Team.new("Temporary Team")
+    temp_team.members = members + other_team.members
+    temp_team
+  end
 end
 
+# cowboys = Team.new("Dallas Cowboys")
+# emmitt = Person.new("Emmitt Smith", 46)     # suppose we're using the Person class from earlier
+
+# cowboys << emmitt                           # will this work?
+
+# p cowboys.members                             # => [#<Person:0x007fe08c209530>]
+# # makes the most sense when working with classes that represent collections
+
 cowboys = Team.new("Dallas Cowboys")
-emmitt = Person.new("Emmitt Smith", 46)     # suppose we're using the Person class from earlier
+cowboys << Person.new("Troy Aikman", 48)
+cowboys << Person.new("Emmitt Smith", 46)
+cowboys << Person.new("Michael Irvin", 49)
 
-cowboys << emmitt                           # will this work?
 
-p cowboys.members                             # => [#<Person:0x007fe08c209530>]
-# makes the most sense when working with classes that represent collections
+niners = Team.new("San Francisco 49ers")
+niners << Person.new("Joe Montana", 59)
+niners << Person.new("Jerry Rice", 52)
+niners << Person.new("Deion Sanders", 47)
 
+dream_team = niners + cowboys # without rewriting the Team#+ method, we would have an array, not a new Team object
+puts dream_team.inspect                     # => #<Team:0x007fac3b9eb878 @name="Temporary Team" ...
