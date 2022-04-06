7831f28b189d6fb2ac38cee152c9be0cde1f2ad0
kkerns20 <kurtkerns20@gmail.com>
2022-04-06 13:18:56 -0500

    [+] notes over shift method

--- lesson_03/fake_operators.rb
+++ lesson_03/fake_operators.rb
@@ -20,3 +20,28 @@ puts "bob is older than kim" if bob > kim
 # Fixed by adding Person#> method
 puts "bob is older" if bob > kim            # => "bob is older"
 puts "bob is older" if bob.>(kim)           # => "bob is older"
+# defining `>` doesn't automatically give us `<`
+
+class Team
+  attr_accessor :name, :members
+
+  def initialize(name)
+    @name = name
+    @members = []
+  end
+
+  def <<(person)
+    return if person.not_yet_18?              # suppose we had Person#not_yet_18?
+    members.push person
+  end
+  # we can build in a guard clause to reject adding members unless some criteria is met
+end
+
+cowboys = Team.new("Dallas Cowboys")
+emmitt = Person.new("Emmitt Smith", 46)     # suppose we're using the Person class from earlier
+
+cowboys << emmitt                           # will this work?
+
+p cowboys.members                             # => [#<Person:0x007fe08c209530>]
+# makes the most sense when working with classes that represent collections
+
