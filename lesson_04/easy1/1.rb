613c2868d3512b2da473e02101fac7b6b9d3d5a2
kkerns20 <kurtkerns20@gmail.com>
2022-04-06 15:31:00 -0500

    [+] solution for easy1 1

---
+++ lesson_04/easy1/1.rb
@@ -0,0 +1,11 @@
+p true.class
+p "hello".class
+p [1, 2, 3, "happy days"].class
+p 142.class
+
+=begin
+`true`, `'hello'`, `[1, 2, 3, "happy days"]`, and `142` are all objects
+Just about everything in Ruby is an object, and we can figure out the 
+class they belong to by calling Object#class method on it.
+  
+=end
