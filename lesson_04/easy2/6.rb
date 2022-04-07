215664d4da5e211f6f3cad99bf060a15cbdd9d3b
kkerns20 <kurtkerns20@gmail.com>
2022-04-06 20:21:15 -0500

    [+] solution for easy2 #6

---
+++ lesson_04/easy2/6.rb
@@ -0,0 +1,20 @@
+class Television
+  def self.manufacturer
+    # method logic
+  end
+
+  def model
+    # method logic
+  end
+end
+
+# Which is a class method and how do you know?
+# How would you call a class method?
+
+=begin
+`manufacturer` is a class method because it is prefixed
+with `self` within the class definition.
+
+to call it, you would nee to use `Television#manufacturer`
+
+=end
