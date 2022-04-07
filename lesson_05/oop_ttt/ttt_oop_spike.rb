3fd9e53060f048bc62eaa796652a9c5b29638206
kkerns20 <kurtkerns20@gmail.com>
2022-04-07 14:03:34 -0500

    [+] human moves added

--- lesson_05/oop_ttt/ttt_oop_spike.rb
+++ lesson_05/oop_ttt/ttt_oop_spike.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 class Board
   INITIAL_MARKER = ' '
 
@@ -9,9 +11,15 @@ class Board
   def get_square_at(key)
     @squares[key]
   end
+
+  def set_square_at(key, marker)
+    @squares[key].marker = marker
+  end
 end
 
 class Square
+  attr_accessor :marker
+  
   def initialize(marker)
     @marker = marker
   end
@@ -22,12 +30,10 @@ class Square
 end
 
 class Player
-  def initialize
-    # maybe a "marker" to keep track of this player's symbol (ie, 'X' or 'O')
-  end
+  attr_reader :marker
 
-  def mark
-
+  def initialize(marker)
+    @marker = marker
   end
 
   def play
@@ -37,10 +43,12 @@ end
 
 # Game Orchestration Engine
 class TTTGame
-  attr_reader :board
+  attr_reader :board, :human, :computer
   
   def initialize
     @board = Board.new
+    @human = Player.new("X")
+    @computer = Player.new("O")
   end
 
   def display_welcome_message
@@ -68,15 +76,27 @@ class TTTGame
     puts ''
   end
 
+  def human_moves
+    puts "Choose a square between 1-9: "
+    square = nil
+    loop do
+      square = gets.chomp.to_i
+      break if (1..9).include?(square)
+      puts "Sorry, that is not a valid choice."
+    end
+
+    binding.pry
+    board.set_square_at(square, human.marker)
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
-      break
-      first_player_moves
+      human_moves
       break if someone_won? || board_full?
 
-      second_player_moves
+      computer_moves
       break if someone_won? || board_full?
     end
     # display_result
