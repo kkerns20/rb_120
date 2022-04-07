050be89d0f3db61e02ece77ac3f92dbb6fe86e90
kkerns20 <kurtkerns20@gmail.com>
2022-04-06 20:18:36 -0500

    [+] solution for easy2 #5

---
+++ lesson_04/easy2/5.rb
@@ -0,0 +1,5 @@
+excited_dog = "excited dog"    # local variable
+@excited_dog = "excited dog"   # instance
+@@excited_dog = "excited dog"  # class variable
+
+# We can tell by how the variables are prefixed
