534e9ef4d65f21dba981136ea045c6169daf8f94
kkerns20 <kurtkerns20@gmail.com>
2022-04-07 13:23:19 -0500

    [+] display board added

--- lesson_05/oop_ttt/ttt_oop_spike.rb
+++ lesson_05/oop_ttt/ttt_oop_spike.rb
@@ -38,17 +38,34 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye!"
   end
 
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |"
+    puts "     |     |"
+    puts ''
+  end
+
   def play
     display_welcome_message
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+    # display_result
     display_goodbye_message
   end
 end
