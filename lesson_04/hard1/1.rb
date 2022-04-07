6f8e773d68631bf6e44c76a067aa6a05bc9ae8f9
kkerns20 <kurtkerns20@gmail.com>
2022-04-07 11:26:22 -0500

    [+] solutions for hard1, hard1 exs complete

--- lesson_04/hard1/1.rb
+++ lesson_04/hard1/1.rb
@@ -25,6 +25,20 @@ class WheeledVehicle
   end
 end
 
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
 class Catamaran
   include Moveable
 
