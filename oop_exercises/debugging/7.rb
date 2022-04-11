5fcf738ab9aba3cecf3593f46df3ec4faaab3152
kkerns20 <kurtkerns20@gmail.com>
2022-04-11 14:11:36 -0500

    [+] solution for debugging #7

---
+++ oop_exercises/debugging/7.rb
@@ -0,0 +1,56 @@
+class BankAccount
+  attr_reader :balance
+
+  def initialize(account_number, client)
+    @account_number = account_number
+    @client = client
+    @balance = 0
+  end
+
+  def deposit(amount)
+    if amount > 0
+      self.balance += amount
+      "$#{amount} deposited. Total balance is $#{balance}."
+    else
+      "Invalid. Enter a positive amount."
+    end
+  end
+
+  def withdraw(amount)
+    if amount > 0 && valid_transaction?(balance - amount)
+      self.balance -= amount
+      "$#{amount} withdrawn. Total balance is $#{balance}."
+    else
+      "Invalid. Enter positive amount less than or equal to current balance ($#{balance})."
+    end
+  end
+
+  def balance=(new_balance)
+    @balance = new_balance
+  end
+
+  def valid_transaction?(new_balance)
+    new_balance >= 0
+  end
+end
+
+# Example
+
+account = BankAccount.new('5538898', 'Genevieve')
+
+                          # Expected output:
+p account.balance         # => 0
+p account.deposit(50)     # => $50 deposited. Total balance is $50.
+p account.balance         # => 50
+p account.withdraw(80)    # => Invalid. Enter positive amount less than or equal to current balance ($50).
+                          # Actual output: $80 withdrawn. Total balance is $50.
+p account.balance         # => 50
+
+=begin
+The problem here is that we are using a setter method for more than one purpose,
+namely, checking the validity of a transaction. Because a setter method always
+returns the value that is assigned, it will always be evaluated to 'true'. The 
+conditional, then, is unable to return `false`. Fix the issue by checking to see if
+the withdrawal is a valid transaction within the #withdraw method. If so, 
+we can invoke the ballance=() method.
+=end
