7f5a9fb6678ac73166340c138d4a7c3d9f7d5158
kkerns20 <kurtkerns20@gmail.com>
2022-04-11 13:42:12 -0500

    [+] solution for debugging #5

---
+++ oop_exercises/debugging/5.rb
@@ -0,0 +1,50 @@
+class File
+  attr_accessor :name, :byte_content
+
+  def initialize(name)
+    @name = name
+  end
+
+  alias_method :read,  :byte_content
+  alias_method :write, :byte_content=
+
+  def copy(target_file_name)
+    target_file = self.class.new(target_file_name)
+    target_file.write(read)
+
+    target_file
+  end
+
+  def to_s
+    # with a defined constant,it can only be called with class::FORMAT
+    # so we needed to include that
+    "#{name}.#{self.class::FORMAT}"
+    # other options would be to create a format method in each subclass
+    # or you could create and instance variable, but that won't convey that the file type won't change
+    # like a constant would
+  end
+end
+
+class MarkdownFile < File
+  FORMAT = :md
+end
+
+class VectorGraphicsFile < File
+  FORMAT = :svg
+end
+
+class MP3File < File
+  FORMAT = :mp3
+end
+
+# Test
+
+blog_post = MarkdownFile.new('Adventures_in_OOP_Land')
+blog_post.write('Content will be added soon!'.bytes)
+
+copy_of_blog_post = blog_post.copy('Same_Adventures_in_OOP_Land')
+
+puts copy_of_blog_post.is_a? MarkdownFile     # true
+puts copy_of_blog_post.read == blog_post.read # true
+
+puts blog_post
